e23285b660a40d8d2fe8a970ddfd8b96f8670e22
Satyakumar BS <bs.satya@gmail.com>
2019-03-24 21:07:24 +0530

    some formatting to improve the presentation of the data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,18 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const chalk = require('chalk')
 
-geocode('hubli', (error, data) => {
+geocode('hubli', (error, geoCodeData) => {
     if (error) {
         return console.log('error', error)
     }
     
-    forecast(data.latitude, data.longitude, (error, data) => {
+    forecast(geoCodeData.latitude, geoCodeData.longitude, (error, forecastData) => {
         if (error) {
             return console.log('Error', error)
         }
-        console.log('Summary: ', data.summary)
+        console.log(chalk.bold('Location:') + geoCodeData.location)
+        console.log(chalk.bold('Latitude:') + geoCodeData.latitude + chalk.bold(' Longitude:'), geoCodeData.longitude)
+        console.log(chalk.bold('Forecast Summary: ') + forecastData.summary)
     })
 })
