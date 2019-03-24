0de5879ddf00c4a2e20fe7ada9d66d0ed49fe1e7
Satyakumar BS <bs.satya@gmail.com>
2019-03-24 08:37:00 +0530

    modified the request api to handle json itself

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,8 +2,6 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/04a9b396fc36725460005c6ce1963a42/37.8267,-122.4233'
 
-request({url: url}, (error, response) => {
-    // console.log(response)
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.currently)
 })
