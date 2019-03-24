a372838449ae97725669b7590e5ac32c38033df5
Satyakumar BS <bs.satya@gmail.com>
2019-03-24 22:07:45 +0530

    refactored the code with object destructuring and object property shorthand

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,15 +1,15 @@
 request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const forecastUrl = 'https://api.darksky.net/forecast/04a9b396fc36725460005c6ce1963a42/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?unit=auto&exclude=[minutely, hourly, alerts, flags]'
+    const url = 'https://api.darksky.net/forecast/04a9b396fc36725460005c6ce1963a42/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?unit=auto&exclude=[minutely, hourly, alerts, flags]'
     
-    request({url: forecastUrl, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('unable to connect to forecast service')
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('unable to find the location')
         } else {
-            const summary = response.body.daily.data[0].summary + ' Its currnetly '+ response.body.currently.temperature + ' degrees out.' + 'There is ' + response.body.currently.precipProbability + '% chances of Rain.'
+            const summary = body.daily.data[0].summary + ' Its currnetly '+ body.currently.temperature + ' degrees out.' + 'There is ' + body.currently.precipProbability + '% chances of Rain.'
             callback(undefined, {
                 summary: summary 
             })
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,18 +1,18 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2F0eWFrdW1hciIsImEiOiJjanRta3MwbHUwNDN0NDNvNWFnbW82bzcyIn0.MPO_epfIzGgWzZYd92LNcQ'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2F0eWFrdW1hciIsImEiOiJjanRta3MwbHUwNDN0NDNvNWFnbW82bzcyIn0.MPO_epfIzGgWzZYd92LNcQ'
 
-    request({url: geoCodeURL, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('unable to conect to the geocode service')
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('unable to find location, try another serch')
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
