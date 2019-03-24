e96c3aae67ff1d4962649401da36642fcfc738d4
Satyakumar BS <bs.satya@gmail.com>
2019-03-24 20:55:21 +0530

    refactored the code

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,15 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('jalahalli', (error, data) => {
-    console.log('error', error)
-    console.log('data', data)
+geocode('hubli', (error, data) => {
+    if (error) {
+        return console.log('error', error)
+    }
+    
     forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error', error)
+        if (error) {
+            return console.log('Error', error)
+        }
         console.log('Summary: ', data.summary)
     })
 })
