d455fbf7f0496217de393eb8f39c2fec7511d8f1
Satyakumar BS <bs.satya@gmail.com>
2019-03-24 08:44:39 +0530

    handling and printing the response from the api

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/04a9b396fc36725460005c6ce1963a42/37.8267,-122.4233'
 
 request({url: url, json: true}, (error, response) => {
-    console.log(response.body.currently)
+    // console.log(response.body.currently)
+    console.log('Its currnetly '+ response.body.currently.temperature + ' degrees out.\n' + 'There is ' + response.body.currently.precipProbability + '% chances of Rain.')
 })
